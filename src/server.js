08b90f2650801f6ca2f73e85d28868d1f9f1e749
Jaesung Park <richdad6208@gmail.com>
2023-08-18 19:35:54 +0900

    change to pug

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = "4000";
 app.use(morgan("dev"));
 app.set("views", process.cwd() + "/src/views");
-app.set("view engine", "ejs");
+app.set("view engine", "pug");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("src"));
 app.use("/assets", express.static("assets"));
